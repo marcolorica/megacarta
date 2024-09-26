d4b0ca628a2b0983bf16a5acd5b28bb8d866bdd8
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 22:57:09 +0200

    fx

--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                Esplora il nostro catalogo
+                <h2>Esplora il nostro catalogo</h2>
             </div>
         </div>
         <div class="row">
