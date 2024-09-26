03ac95b0888e3218a6e02b2bd3f4bb515207c81f
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 23:57:14 +0200

    fx

--- page-catalogo.php
+++ page-catalogo.php
@@ -75,7 +75,7 @@
                             <p class="product-title"><?= $p->name ?></p>
                             <div class="product-info">
                                 <span class="mg-price">€<?= $p->price ?></span>
-                                <button class="btn btn-ouline-success"><i class="fa-solid fa-shopping-cart me-2"></i>Aggiungi al carrello</button>
+                                <button class="btn btn-outline-success"><i class="fa-solid fa-shopping-cart me-2"></i>Aggiungi al carrello</button>
                             </div>
                         </div>
                     </div>
