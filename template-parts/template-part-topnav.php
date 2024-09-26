58fddf905d19a7d3a243eea3f41b5c3ee649bf00
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 22:58:05 +0200

    fx

--- template-parts/template-part-topnav.php
+++ template-parts/template-part-topnav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-">
+<nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">
       <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
