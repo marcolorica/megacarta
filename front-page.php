2b9b1477c2a9b0a062aa97b01ea073602242af0c
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 22:41:48 +0200

    fx

--- front-page.php
+++ front-page.php
@@ -1,4 +1,9 @@
 <?php get_header(); ?>
+
+<?php
+    $cats = get_mc_categories();
+?>
+
 <section class="intestazione">
     <div class="container-fluid">
         <div class="row">
@@ -7,6 +12,28 @@
                     <h1>MEGACARTA</h1>
                 </div>
             </div>
+        </div>
+    </div>
+</section>
+
+<section class="categories">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                Esplora il nostro catalogo
+            </div>
+        </div>
+        <div class="row">
+            <?php foreach($cats as $c): ?>
+                <div class="col-12 col-md-4">
+                    <a href="/catalogo/<?= $c->lug ?>" class="link-cat">
+                        <div class="card-categoria">
+                            <img src="<?= $c->img ?>" class="img-cat">
+                            <p class="title-cat"><?= $c->name ?></p>
+                        </div>
+                    </a>
+                </div>
+            <? endforeach; ?>
         </div>
     </div>
 </section>
--- functions.php
+++ functions.php
@@ -24,4 +24,34 @@ function mc_wp_enqueue_scripts() {
     wp_enqueue_script('swal2-js', "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js", [], "11.10.0");
 }
 
+function get_mc_categories() {
+	$args = [
+		'taxonomy'   => 'product_cat',
+		'hide_empty' => false,
+	];
+
+	$product_categories = get_terms($args);
+
+	$res = [];
+
+	if(!empty($product_categories) && !is_wp_error($product_categories)) {
+		foreach($product_categories as $category) {
+			$thumbnail_id = get_term_meta($category->term_id, 'thumbnail_id', true);
+            
+			$img = '';
+
+            if($thumbnail_id)
+                $image_url = wp_get_attachment_url($thumbnail_id);
+
+			$res[] = (object) [
+				'name' => $category->name,
+				'slug' => $category->slug,
+				'img' => $img
+			];
+		}
+	}
+
+	return $res;
+}
+
 add_action('wp_enqueue_scripts', 'mc_wp_enqueue_scripts');
