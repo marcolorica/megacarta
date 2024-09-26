5bc38e91cf24f3d9de069efacba5dbba606547ea
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 22:32:08 +0200

    fx

--- template-parts/template-part-topnav.php
+++ template-parts/template-part-topnav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-dark text-white">
+<nav class="navbar navbar-expand-lg bg-">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">
       <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
@@ -12,13 +12,13 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <!-- <a class="nav-link active" aria-current="page" href="#">Chi siamo</a> -->
-          <a class="nav-link" href="#">Chi siamo</a>
+          <a class="nav-link text-white" href="#">Chi siamo</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Prodotti</a>
+          <a class="nav-link text-white" href="#">Prodotti</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contatti</a>
+          <a class="nav-link text-white" href="#">Contatti</a>
         </li>
       </ul>
     </div>
