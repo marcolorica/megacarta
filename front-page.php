12a6f12e7411d2c9070539380ce7666e7fea6c86
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 22:16:15 +0200

    fx

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="mg-overlay h-100 w-100"></div>
+                <div class="mc-overlay h-100 w-100"></div>
                 <img class="w-100" src="<?= get_stylesheet_directory_uri() . '/assets/images/megacarta-bg.webp' ?>">
             </div>
         </div>
