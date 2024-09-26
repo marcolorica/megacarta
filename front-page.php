8e1016dfd370c317e2df5ff56a2ff8eabe2ea6d6
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 22:26:05 +0200

    fx

--- front-page.php
+++ front-page.php
@@ -1,20 +1,9 @@
 <?php get_header(); ?>
-
-<style>
-    .intestazione {
-        background-image: url('/assets/images/megacarta-bg.webp');
-    }
-    .mc-overlay {
-        background-color: #ffffff60;
-        min-height: 80vh;
-    }
-</style>
-
 <section class="intestazione">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="mc-overlayw-100">
+                <div class="mc-overlay w-100">
                     <h1>MEGACARTA</h1>
                 </div>
             </div>
