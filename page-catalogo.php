c6768bb3f53f92759e5fb5f4752e1ce52a2285a9
marcolorica <marcolinoh.ml@gmail.com>
2024-09-26 23:55:26 +0200

    fx

--- page-catalogo.php
+++ page-catalogo.php
@@ -63,22 +63,22 @@
             </div>
             <div class="col-12 col-md-9 products">
                 <?php foreach($products as $p): ?>
-                    <a href="<?= $p->url ?>">
-                        <div class="row">
-                            <div class="col-3 ps-0">
+                    <div class="row">
+                        <div class="col-3 p-0">
+                            <a href="<?= $p->url ?>">
                                 <div class="img-container">
                                     <img src="<?= $p->img ?>" class="w-100">
                                 </div>
-                            </div>
-                            <div class="col-9">
-                                <p class="product-title"><?= $p->name ?></p>
-                                <div class="product-info">
-                                    <span class="mg-price">€<?= $p->price ?></span>
-                                    <button class="btn-success"><i class="fa-solid fa-shopping-cart me-2"></i>Aggiungi al carrello</button>
-                                </div>
+                            </a>
+                        </div>
+                        <div class="col-9">
+                            <p class="product-title"><?= $p->name ?></p>
+                            <div class="product-info">
+                                <span class="mg-price">€<?= $p->price ?></span>
+                                <button class="btn btn-ouline-success"><i class="fa-solid fa-shopping-cart me-2"></i>Aggiungi al carrello</button>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 <?php endforeach; ?>
             </div>
         </div>
