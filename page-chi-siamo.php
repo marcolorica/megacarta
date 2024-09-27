bd0dacd4c455da7467f18dfd659da74f19ac2c19
marcolorica <marcolinoh.ml@gmail.com>
2024-09-27 11:08:00 +0200

    fx

--- page-chi-siamo.php
+++ page-chi-siamo.php
@@ -14,7 +14,7 @@
 
 <section class="mappa mt-5">
     <div class="container">
-        <div class="row">
+        <div class="row mb-3">
             <div class="col-12 col-md-6">
                 <h3>MEGACARTA SRL: La Tua Soluzione Completa per Rifornimenti di Carta, Plastica e Alluminio a Roma</h3>
 
@@ -26,24 +26,30 @@
                 <img src="<?= get_stylesheet_directory_uri() . '/assets/images/chi-siamo-1.jpg' ?>" class="w-100">
             </div>
         </div>
-        <div class="row row-md-reverse">
+        <div class="row mb-3 row-reverse d-none d-md-block">
             <div class="col-12 col-md-6">
                 <p>Oltre alla qualità dei prodotti, MEGACARTA SRL si impegna a rispettare le normative ambientali vigenti, proponendo anche soluzioni ecosostenibili per chi desidera ridurre l’impatto ambientale. Offriamo infatti prodotti in carta riciclata, biodegradabile, nonché alternative plastiche compostabili, senza mai compromettere l'efficienza e la funzionalità.</p>
                 <p>Siamo il partner ideale per uffici, negozi, ristoranti, alberghi e tutte le attività che necessitano di un rifornimento costante di carta, plastica e alluminio. Grazie alla nostra esperienza e alla capacità di comprendere le specifiche esigenze di ciascun cliente, ci adattiamo perfettamente a diverse tipologie di business, garantendo una fornitura continua e senza interruzioni.</p>
             </div>
 
             <div class="col-12 col-md-6">
-                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/' ?>" class="w-100">
+                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/chi-siamo-2.png' ?>" class="w-100">
             </div>
         </div>
-        <div class="row">
+        <div class="row mb-3 d-block d-md-none">
             <div class="col-12 col-md-6">
-                <p>Il nostro team è composto da professionisti altamente qualificati, pronti ad assistere i clienti in ogni fase, dalla scelta dei prodotti alla fase di consegna. Investiamo continuamente in formazione e innovazione, per offrire un servizio di distribuzione sempre più efficiente, riducendo i tempi di attesa e migliorando l'interazione con i nostri clienti.</p>
-                <p>Affidarsi a MEGACARTA SRL significa scegliere un servizio di distribuzione che unisce qualità, efficienza e sostenibilità. Con un approccio orientato al cliente e una logistica ottimizzata, siamo pronti a soddisfare tutte le richieste di forniture di carta, plastica e alluminio in tutta la città di Roma, contribuendo al successo delle aziende che si affidano a noi.</p>
+                <p>Oltre alla qualità dei prodotti, MEGACARTA SRL si impegna a rispettare le normative ambientali vigenti, proponendo anche soluzioni ecosostenibili per chi desidera ridurre l’impatto ambientale. Offriamo infatti prodotti in carta riciclata, biodegradabile, nonché alternative plastiche compostabili, senza mai compromettere l'efficienza e la funzionalità.</p>
+                <p>Siamo il partner ideale per uffici, negozi, ristoranti, alberghi e tutte le attività che necessitano di un rifornimento costante di carta, plastica e alluminio. Grazie alla nostra esperienza e alla capacità di comprendere le specifiche esigenze di ciascun cliente, ci adattiamo perfettamente a diverse tipologie di business, garantendo una fornitura continua e senza interruzioni.</p>
             </div>
 
             <div class="col-12 col-md-6">
-                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/' ?>" class="w-100">
+                <img src="<?= get_stylesheet_directory_uri() . '/assets/images/chi-siamo-2.png' ?>" class="w-100">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-12">
+                <p>Il nostro team è composto da professionisti altamente qualificati, pronti ad assistere i clienti in ogni fase, dalla scelta dei prodotti alla fase di consegna. Investiamo continuamente in formazione e innovazione, per offrire un servizio di distribuzione sempre più efficiente, riducendo i tempi di attesa e migliorando l'interazione con i nostri clienti.</p>
+                <p>Affidarsi a MEGACARTA SRL significa scegliere un servizio di distribuzione che unisce qualità, efficienza e sostenibilità. Con un approccio orientato al cliente e una logistica ottimizzata, siamo pronti a soddisfare tutte le richieste di forniture di carta, plastica e alluminio in tutta la città di Roma, contribuendo al successo delle aziende che si affidano a noi.</p>
             </div>
         </div>
     </div>
