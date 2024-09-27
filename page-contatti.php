10163cdf0c18454ab29dfe79e10fbeee4e32963b
marcolorica <marcolinoh.ml@gmail.com>
2024-09-27 11:12:28 +0200

    fx

--- page-contatti.php
+++ page-contatti.php
@@ -15,7 +15,7 @@
 <section class="contacts mt-4 mb-5">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-6 text-end mb-3">
+            <div class="col-12 col-md-6 text-end mb-5">
                 <label class="w-100 text-start" for="name">Nome <span class="text-danger">*</span></label>
                 <input type="text" name="name" class="form-control mb-3">
 
@@ -31,7 +31,7 @@
                 <button class="btn btn-primary"><i class="fa-regular fa-paper-plane me-2"></i>Invia</button>
             </div>
             <div class="col-12 col-md-6">
-                <h3 class="text-center text-md-start">Ci troviamo qui</h3>
+                <h3 class="text-center text-md-start mb-3">Ci troviamo qui</h3>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2973.787048369707!2d12.632556876784765!3d41.811347671249194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13258632ef4dfd4b%3A0x98bb2a73478684b1!2sMEGA%20CARTA%20SRL!5e0!3m2!1sit!2sit!4v1727395814061!5m2!1sit!2sit" class="w-100" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 
                 <div class="socials mt-3 <?= wp_is_mobile() ? 'mobile' : '' ?>">
