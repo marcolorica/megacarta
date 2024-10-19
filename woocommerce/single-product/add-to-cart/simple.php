a04476ad4d99f1d41ecc49ae5a3823d8b70aca05
marcolorica <marcolinoh.ml@gmail.com>
2024-10-19 18:08:37 +0200

    fx

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -15,45 +15,45 @@
  * @version 7.0.1
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
-if ( ! $product->is_purchasable() ) {
+if(!$product->is_purchasable())
 	return;
-}
 
-echo wc_get_stock_html( $product ); // WPCS: XSS ok.
+echo wc_get_stock_html($product); // WPCS: XSS ok.
 
-if ( $product->is_in_stock() && cor_is_user_logged_in()) : ?>
-	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+if($product->is_in_stock()) :
+?>
+	<?php do_action('woocommerce_before_add_to_cart_form'); ?>
 
-	<form class="cart d-flex" action="<?= esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ) ?>" method="post" enctype='multipart/form-data'>
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+	<form class="cart d-flex" action="<?= esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink())) ?>" method="post" enctype='multipart/form-data'>
+		<?php do_action( 'woocommerce_before_add_to_cart_button'); ?>
 
 		<?php
-		do_action( 'woocommerce_before_add_to_cart_quantity' );
+		do_action( 'woocommerce_before_add_to_cart_quantity');
 
 		woocommerce_quantity_input(
 			array(
-				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-				'classes'     => apply_filters( 'woocommerce_quantity_input_classes', array('input-text', 'qty', 'text'), $product )
+				'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+				'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+				'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				'classes'     => apply_filters('woocommerce_quantity_input_classes', array('input-text', 'qty', 'text'), $product)
 			)
 		);
 
-		do_action( 'woocommerce_after_add_to_cart_quantity' );
+		do_action('woocommerce_after_add_to_cart_quantity');
 
 		?>
 
-		<button type="submit" name="add-to-cart" value="<?= esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt<?= esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><i class="fa-solid fa-cart-shopping me-2"></i>Aggiungi al carrello</button>
+		<button type="submit" name="add-to-cart" value="<?= esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt<?= esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><i class="fa-solid fa-cart-shopping me-2"></i>Aggiungi al carrello</button>
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+		<?php do_action('woocommerce_after_add_to_cart_button'); ?>
 		<br>
 		<br>
 	</form>
 
-	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+	<?php do_action('woocommerce_after_add_to_cart_form'); ?>
 
 <?php endif; ?>
