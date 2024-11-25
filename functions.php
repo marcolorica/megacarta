b2b6a104319e2f4c80cb7f94fe46249e1a520384
marcolorica <marcolinoh.ml@gmail.com>
2024-11-25 15:34:55 +0100

    fx

--- functions.php
+++ functions.php
@@ -752,3 +752,14 @@ function mc_get_categories() {
 
     return $result;
 }
+
+function filter_woocommerce_placeholder_img_src($src) {
+    global $product;
+
+    if(is_a($product, 'WC_Product'))
+        $src = get_stylesheet_directory_uri() . '/assets/images/megacarta-logo.webp';
+    
+    return $src;
+}
+
+add_filter('woocommerce_placeholder_img_src', 'filter_woocommerce_placeholder_img_src', 10, 1);
