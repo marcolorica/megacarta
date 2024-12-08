bc406c153268110f187aa639a343440d05bad249
marcolorica <marcolinoh.ml@gmail.com>
2024-12-08 19:38:42 +0100

    fx

--- template-parts/area-admin/prodotti.php
+++ template-parts/area-admin/prodotti.php
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <h2>Prdotti</h2>
+                <h2>Prodotti</h2>
             </div>
             <div class="col-12">
                 <div class="input-group mb-3" id="adminSearch">
                     <input type="text" class="form-control" placeholder="Cerca prodotti...">
-                    <button class="btn btn-outline-primary" type="button"><i class="fa-solid fa-magnyfing-glass"></i></button>
+                    <button class="btn btn-outline-primary" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
                 </div>
             </div>
         </div>
