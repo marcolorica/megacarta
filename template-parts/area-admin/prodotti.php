4522195b95fbbce64a198efe95c130f30efd8bb5
marcolorica <marcolinoh.ml@gmail.com>
2024-12-09 23:10:02 +0100

    fx

--- template-parts/area-admin/prodotti.php
+++ template-parts/area-admin/prodotti.php
@@ -17,7 +17,7 @@
     <input type="hidden" name="num_page" value="<?= $numPage ?>">
 </form>
 
-<section class="admin-body pt-5">
+<section class="admin-body py-5">
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
