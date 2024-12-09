bab21c29974e4cf8b93ceca37e0b6823a01624c3
marcolorica <marcolinoh.ml@gmail.com>
2024-12-09 22:36:39 +0100

    fx

--- template-parts/area-admin/area-admin.php
+++ template-parts/area-admin/area-admin.php
@@ -2,9 +2,9 @@
 
 <section class="mt-5" style="min-height:100vh">
     <div class="container pt-5">
-        <div class="row">
-            <div class="col-12">
-                <img src="<?= mc_get_logo_src(true); ?>">
+        <div class="row mt-5">
+            <div class="col-12 d-flex justify-content-center mt-5 pt-5">
+                <img class="w-25" src="<?= mc_get_logo_src(true); ?>" style="opacity:.1">
             </div>
         </div>
     </div>
