d3687b40cb949b45fae4b35749931ae9b9e8ec20
marcolorica <marcolinoh.ml@gmail.com>
2025-01-02 14:48:40 +0100

    fx

--- page-admin.php
+++ page-admin.php
@@ -11,6 +11,14 @@
                 <h3 class="mb-3">Inserisci le credenziali di accesso</h3>
 
                 <input type="email" class="form-control mb-3" name="email" form="form-login" placeholder="Email">
+
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <button class="btn btn-outline-secondary" type="button" onclick="togglePasswordType(el)">
+                        <i class="fa-regular fa-eye"></i>
+                    </button>
+                </div>
+
                 <input type="password" class="form-control mb-3" name="password" form="form-login" placeholder="Password">
 
                 <button class="btn btn-danger float-end" type="submit" form="form-login">Accedi</button>
@@ -18,6 +26,22 @@
         </div>
     </div>
 </section>
+
+<script>
+    function togglePasswordType(el) {
+        let $psw = jQuery(el).prev();
+        let $icon = jQuery(el).find('i');
+
+        if($psw.attr('type') == 'text') {
+            $psw.attr('type', 'password');
+            $icon.removeClass('fa-eye-slash').addClass('fa-eye');
+        }
+        else {
+            $psw.attr('type', 'text');
+            $icon.removeClass('fa-eye').addClass('fa-eye-slash');
+        }
+    }
+</script>
 
 <?php if(isset($_SESSION['error_login'])) : ?>
     <script>
