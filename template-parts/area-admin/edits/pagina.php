d7d02e9174b94bd0b6b0e44bb2f14d1b0ff42e9a
marcolorica <marcolinoh.ml@gmail.com>
2025-01-04 13:29:26 +0100

    fx

--- template-parts/area-admin/edits/pagina.php
+++ template-parts/area-admin/edits/pagina.php
@@ -40,12 +40,16 @@
                             <h4 class="mb-3">Categorie in vetrina <span class="text-danger">*</span></h4>
 
                             <div class="accordion mb-4" id="accordionEditPage">
-                                <?php foreach($categories as $cid => $c) : $cid = str_replace('c-', '', $cid); ?>
+                                <?php
+                                    foreach($categories as $cid => $c) : 
+                                        $cid = str_replace('c-', '', $cid);
+                                        $opened = count($categories->children) ? !empty(array_intersect($datas->categories, array_column(json_decode(json_encode($categories->children), true), 'slug'))) : in_array($c->slug, $datas->categories ?: []);
+                                ?>
                                     <div class="accordion-item">
                                         <h2 class="accordion-header">
-                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse<?= $cid ?>" aria-expanded="false" aria-controls="panelsStayOpen-collapse<?= $cid ?>"><?= $c->name ?></button>
+                                            <button class="accordion-button <?= $opened ? '' : 'collapsed' ?>" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse<?= $cid ?>" aria-expanded="<?= $opened ? 'true' : 'false' ?>" aria-controls="panelsStayOpen-collapse<?= $cid ?>"><?= $c->name ?></button>
                                         </h2>
-                                        <div id="panelsStayOpen-collapse<?= $cid ?>" class="accordion-collapse collapse">
+                                        <div id="panelsStayOpen-collapse<?= $cid ?>" class="accordion-collapse collapse <?= $opened ? 'show' : '' ?>">
                                             <div class="accordion-body">
                                                 <?php foreach($c->children as $sid => $subc) : ?>
                                                     <label for="cat-<?= "$cid-$sid" ?>">
