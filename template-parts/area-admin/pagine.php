9f8f8cc109bfe363b75c4e6e1afcc78d46ec243f
marcolorica <marcolinoh.ml@gmail.com>
2025-01-04 12:20:07 +0100

    fx

--- template-parts/area-admin/pagine.php
+++ template-parts/area-admin/pagine.php
@@ -25,7 +25,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <h2 class="mb-3">Pagine</h2>
+                <h2 class="mb-5">Pagine</h2>
             </div>
 
             <div class="col-12 body-content">
