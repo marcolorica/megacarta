d93ae1aa793eab469bd306deea22f8d5c9553457
marcolorica <marcolinoh.ml@gmail.com>
2025-01-04 13:16:13 +0100

    fx

--- inc/post.php
+++ inc/post.php
@@ -28,47 +28,35 @@ function admin_login_action_handler() {
 function admin_save_page_edits() {
     $request = (object) $_POST;
 
-    $pagina = $request->pagina;
+    $to_update = [
+        'home_main_img' => null,
+        'home_categories' => null,
+        'home_map_title' => null,
+        'home_map_text' => null,
+        'chi_siamo_main_img' => null,
+        'chi_siamo_title_1' => null,
+        'chi_siamo_text_1' => null,
+        'chi_siamo_img_1' => null,
+        'chi_siamo_title_2' => null,
+        'chi_siamo_text_2' => null,
+        'chi_siamo_img_2' => null,
+        'chi_siamo_content_1' => null,
+        'chi_siamo_content_2' => null,
+        'contatti_main_img' => null,
+        'contacts_phone' => null,
+        'contacts_whatsapp' => null,
+        'contacts_email' => null
+    ];
 
-    $home_main_img = $request->home_main_img ?? null;
-    $home_categories = $request->home_categories ?? null;
-    $home_map_title = $request->home_map_title ?? null;
-    $home_map_text = $request->home_map_text ?? null;
-    $chi_siamo_main_img = $request->chi_siamo_main_img ?? null;
-    $chi_siamo_title_1 = $request->chi_siamo_title_1 ?? null;
-    $chi_siamo_text_1 = $request->chi_siamo_text_1 ?? null;
-    $chi_siamo_img_1 = $request->chi_siamo_img_1 ?? null;
-    $chi_siamo_title_2 = $request->chi_siamo_title_2 ?? null;
-    $chi_siamo_text_2 = $request->chi_siamo_text_2 ?? null;
-    $chi_siamo_img_2 = $request->chi_siamo_img_2 ?? null;
-    $chi_siamo_content_1 = $request->chi_siamo_content_1 ?? null;
-    $chi_siamo_content_2 = $request->chi_siamo_content_2 ?? null;
-    $contatti_main_img = $request->contatti_main_img ?? null;
-    $contacts_phone = $request->contacts_phone ?? null;
-    $contacts_whatsapp = $request->contacts_whatsapp ?? null;
-    $contacts_email = $request->contacts_email ?? null;
+    foreach($to_update as $name) {
+        if($name == 'home_categories') {
+            var_dump($request->$name);die;
+        }
 
-    var_dump($home_categories);die;
-
-    update_option('mc_home_main_img', $home_main_img);
-    update_option('mc_home_categories', $home_categories);
-    update_option('mc_home_map_title', $home_map_title);
-    update_option('mc_home_map_text', $home_map_text);
-    update_option('mc_chi_siamo_main_img', $chi_siamo_main_img);
-    update_option('mc_chi_siamo_title_1', $chi_siamo_title_1);
-    update_option('mc_chi_siamo_text_1', $chi_siamo_text_1);
-    update_option('mc_chi_siamo_img_1', $chi_siamo_img_1);
-    update_option('mc_chi_siamo_title_2', $chi_siamo_title_2);
-    update_option('mc_chi_siamo_text_2', $chi_siamo_text_2);
-    update_option('mc_chi_siamo_img_2', $chi_siamo_img_2);
-    update_option('mc_chi_siamo_content_1', $chi_siamo_content_1);
-    update_option('mc_chi_siamo_content_2', $chi_siamo_content_2);
-    update_option('mc_contatti_main_img', $contatti_main_img);
-    update_option('mc_contacts_phone', $contacts_phone);
-    update_option('mc_contacts_whatsapp', $contacts_whatsapp);
-    update_option('mc_contacts_email', $contacts_email);
+        update_option('mc_' . $name, ($request->$name ?? null));
+    }
 
     $_SESSION['save_success'] = true;
-    wp_redirect('/area-admin/pagine/pagina?slug=' . $pagina);
+    wp_redirect('/area-admin/pagine/pagina?slug=' . $request->pagina);
     exit();
 }
