44ddde8e6987c6e92f2ab663b4596b9b9a692697
marcolorica <marcolinoh.ml@gmail.com>
2025-01-04 13:17:08 +0100

    fx

--- inc/post.php
+++ inc/post.php
@@ -29,23 +29,23 @@ function admin_save_page_edits() {
     $request = (object) $_POST;
 
     $to_update = [
-        'home_main_img' => null,
-        'home_categories' => null,
-        'home_map_title' => null,
-        'home_map_text' => null,
-        'chi_siamo_main_img' => null,
-        'chi_siamo_title_1' => null,
-        'chi_siamo_text_1' => null,
-        'chi_siamo_img_1' => null,
-        'chi_siamo_title_2' => null,
-        'chi_siamo_text_2' => null,
-        'chi_siamo_img_2' => null,
-        'chi_siamo_content_1' => null,
-        'chi_siamo_content_2' => null,
-        'contatti_main_img' => null,
-        'contacts_phone' => null,
-        'contacts_whatsapp' => null,
-        'contacts_email' => null
+        'home_main_img',
+        'home_categories',
+        'home_map_title',
+        'home_map_text',
+        'chi_siamo_main_img',
+        'chi_siamo_title_1',
+        'chi_siamo_text_1',
+        'chi_siamo_img_1',
+        'chi_siamo_title_2',
+        'chi_siamo_text_2',
+        'chi_siamo_img_2',
+        'chi_siamo_content_1',
+        'chi_siamo_content_2',
+        'contatti_main_img',
+        'contacts_phone',
+        'contacts_whatsapp',
+        'contacts_email'
     ];
 
     foreach($to_update as $name) {
