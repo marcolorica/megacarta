f5de7e31c1b3fb82761a1866d47d17304daa566c
marcolorica <marcolinoh.ml@gmail.com>
2025-01-06 23:59:36 +0100

    fx

--- template-parts/area-admin/impostazioni.php
+++ template-parts/area-admin/impostazioni.php
@@ -7,6 +7,10 @@
 <section class="admin-body pt-5">
     <div class="container">
         <div class="row">
+            <div class="col-12 text-end">
+                <button type="submit" class="btn btn-success" form="form-cat">Salva</button>
+            </div>
+            
             <div class="col-12 text-center">
                 <h2 class="mb-3">Impostazioni</h2>
             </div>
