87b31336f59af23c96b04a091dadc4171798f29e
marcolorica <marcolinoh.ml@gmail.com>
2025-01-07 15:22:51 +0100

    fx

--- inc/functions.php
+++ inc/functions.php
@@ -305,6 +305,8 @@ function mc_upload_image_in_theme($img_name, $img_tmp_name, $dir = '') {
 function mc_get_product($product_id) {
     $product = wc_get_product($product_id);
 
+    var_dump($product->get_stock_quantity());die;
+
     if(!$product)
         return null;
 
