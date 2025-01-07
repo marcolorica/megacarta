15bfb7a7be8d93725079f1cd04eb2dafa23b66db
marcolorica <marcolinoh.ml@gmail.com>
2025-01-07 15:20:30 +0100

    fx

--- inc/post.php
+++ inc/post.php
@@ -256,8 +256,6 @@ function admin_save_product_edits() {
         $variants[] = $_variant;
     }
 
-    var_dump(intval($request->product_qty));die;
-
     if(!$product_id) {
         $product = new WC_Product_Simple();
 		$product->set_name($request->product_code);
