859fe450bf9e8fff77e77c711120cbab44194c96
marcolorica <marcolinoh.ml@gmail.com>
2025-01-09 12:59:36 +0100

    fx

--- inc/filters.php
+++ inc/filters.php
@@ -1,6 +1,7 @@
 <?php
 
 add_filter('woocommerce_placeholder_img_src', 'filter_woocommerce_placeholder_img_src', 10, 1);
+add_filter( 'woocommerce_get_availability_text', 'filter_woocommerce_product_availability', 10, 2);
 
 function filter_woocommerce_placeholder_img_src($src) {
     global $product;
@@ -9,4 +10,11 @@ function filter_woocommerce_placeholder_img_src($src) {
         $src = mc_get_logo_src();
     
     return $src;
+}
+
+function filter_woocommerce_product_availability($availability, $product) {
+    if(is_product())
+        return '';
+
+    return $availability;
 }
