b05d7faa0e4794c170c919f21b74825cda86f4bb
marcolorica <marcolinoh.ml@gmail.com>
2025-01-09 13:12:52 +0100

    fx

--- template-parts/wc/product-variants.php
+++ template-parts/wc/product-variants.php
@@ -1,13 +1,13 @@
 <?php $mg_product = mc_get_product(get_the_ID()); ?>
 
 <?php if(count($mg_product->variants)) : ?>
-    <div class="mg-variants">
+    <div class="mg-variants mb-4">
         <?php foreach($mg_product->variants as $i => $v) : ?>
-            <div class="mg-variant" onclick="setVariant(<?= $v->id ?>)">
+            <div class="mg-variant <?= $mg_product->only_variants && !$i ? 'selected' : '' ?>" onclick="setVariant(<?= $v->id ?>)">
                 <?php if($v->img) : ?>
                     <img src="<?= $v->img ?>">
                 <?php endif; ?>
-                <span class="v-name"><?= $v->name ?></span>
+                <span class="v-name me-2"><?= $v->name ?></span>
                 <span class="v-price"><?= $v->price ?></span>
             </div>
         <?php endforeach; ?>
