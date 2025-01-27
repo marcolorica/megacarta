3a1c1e1cbaa84b31dbe4f97b76535ff3a8cce5fb
marcolorica <marcolinoh.ml@gmail.com>
2025-01-28 00:07:00 +0100

    fx

--- template-parts/area-admin/ordini.php
+++ template-parts/area-admin/ordini.php
@@ -65,7 +65,6 @@
                         <div class="col-2 d-flex justify-content-end align-items-center">
                             <a class="btn azione-mc btn-outline-success me-3" href="#"><i class="fa-solid fa-eye fa-fw text-success"></i></a>
                             <a class="btn azione-mc btn-outline-primary me-3" href="/area-admin/ordini/ordine?id=<?= $o->id ?>"><i class="fa-solid fa-pencil fa-fw text-primary"></i></a>
-                            <a class="btn azione-mc btn-outline-danger" role="button" onclick="adminDeleteOrder(<?= $o->id ?>)"><i class="fa-solid fa-trash-can fa-fw"></i></a>
                         </div>
                     </div>
                 <?php endforeach; ?>
