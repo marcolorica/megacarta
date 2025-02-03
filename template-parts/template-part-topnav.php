4e29b9d2cd99d001b89a613e619655e9215bcf70
marcolorica <marcolinoh.ml@gmail.com>
2025-02-03 16:03:00 +0100

    fx

--- template-parts/template-part-topnav.php
+++ template-parts/template-part-topnav.php
@@ -5,7 +5,7 @@
     $perPage = isset($request->per_page) ? (int) $request->per_page : 10;
     $numPage = isset($request->num_page) ? $request->num_page : 1;
     
-    var_dump(mc_get_cart_count());die;
+    var_dump();die;
     if(!mg_is_admin_area()) :
 ?>
 
