df726e19109ed82306d4655f0ef0d9332bb49b82
marcolorica <marcolinoh.ml@gmail.com>
2025-02-03 17:02:19 +0100

    fx

---
+++ page-order-received.php
@@ -0,0 +1,21 @@
+<?php
+    get_header();
+    $url_parts = explode('/', $_SERVER['REQUEST_URI']);
+    $order_id = intval($url_parts[count($url_parts) - 1]);
+
+    $order = wc_get_order($order_id);
+    $customer_name = $order ? $order->get_billing_first_name() : '';
+?>
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Grazie <?= $customerName ?> per aver acquistato da noi!</h1>
+                <h3 class="text-center">Riceverai una mail con i dettagli dell'ordine</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
