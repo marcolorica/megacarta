32e01f58beb76ad0bf0f918afa986184bb7f319c
marcolorica <marcolinoh.ml@gmail.com>
2025-02-03 16:27:04 +0100

    fx

--- inc/filters.php
+++ inc/filters.php
@@ -49,7 +49,5 @@ function filter_woocommerce_cart_product_variants($item_data, $cart_item) {
 
 function filter_woocommerce_cart_item_img($image, $cart_item, $cart_item_key) {
     $product_id = $cart_item['product_id'];
-
-    var_dump($product_id);die;
-    return mc_get_product_image($product_id);
+    return '<img src="' . esc_url(mc_get_product_image($product_id)) . '" alt="' . esc_attr($product->get_name()) . '" />';
 }
