59c8e63b634d6a362051815616c21a622ec1a0be
marcolorica <marcolinoh.ml@gmail.com>
2025-02-04 12:55:50 +0100

    fx

--- inc/functions.php
+++ inc/functions.php
@@ -202,6 +202,8 @@ function mc_get_orders($term = null, $perPage = 10, $_order = 'piu-recenti', $nu
     $query = new WC_Order_Query($args);
     $_orders = $query->get_orders();
 
+    var_dump($_orders);die;
+
     $statuses = wc_get_order_statuses();
     $statusColors = [
         'pending' => 'warning',
