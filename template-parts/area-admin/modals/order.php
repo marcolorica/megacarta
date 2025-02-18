6e46f026c78a404d40b42d3afb1dac1b856e1e39
marcolorica <marcolinoh.ml@gmail.com>
2025-02-19 00:25:52 +0100

    fx

--- template-parts/area-admin/modals/order.php
+++ template-parts/area-admin/modals/order.php
@@ -6,6 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+                <?= var_dump($order, $args); ?>
                 <select id="order_status">
                     <option <?= $order->status == 'pending' ? 'selected' : '' ?> value="pending">In attesa di pagamento</option>
                     <option <?= $order->status == 'processing' ? 'selected' : '' ?> value="processing">In lavorazione</option>
